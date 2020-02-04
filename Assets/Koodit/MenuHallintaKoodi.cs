01ac53b968fbca291e307b41bb9e0b4f106db32e
Justus Juutilainen <justus.juutilainen@elisanet.fi>
2020-02-04 15:28:03 +0000

    Menu nappi ja aloitus scenen runko jotta saadaan testattua quit nappula.

---
+++ Assets/Koodit/MenuHallintaKoodi.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHallintaKoodi : MonoBehaviour
+{
+    public GameObject Menu = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void JatkaPainettu()
+    {
+        GameObject.Destroy(Menu);        
+    }
+
+    public void AlustaPainettu ()
+    {        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Pisteet.pisteet = 100;
+    }
+
+    public void LopetaPainettu()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+}
---
+++ Assets/Koodit/MenuKoodi.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuKoodi : MonoBehaviour
+{
+        
+    public GameObject MenuTaulu = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+               
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NappiPainettu()
+    {
+        GameObject apumenu = Instantiate(this.MenuTaulu, new Vector3(149, 197, 0), Quaternion.identity);
+        apumenu.name = "menu";
+    }
+
+
+   
+}
