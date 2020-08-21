ca54138d06736a2d1344df173a65fb21209edb1d
Justus Juutilainen <justus.juutilainen@elisanet.fi>
2020-08-21 16:24:04 +0000

    Palikan sijoitus ohjeen korjauksia

---
+++ Assets/Koodit/Ohje1Seuraava.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ohje1Seuraava : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void seuraava1Painettu()
+    {
+        SceneManager.LoadScene("Ohje 2");
+    }
+
+    public void seuraava2Painettu()
+    {
+        SceneManager.LoadScene("Ohje 3");
+    }
+
+}
